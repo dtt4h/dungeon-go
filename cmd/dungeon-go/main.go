c04eae3359c5cfd642a9dba65692b2dc568cd593
dtt4h <dtt4h322@gmail.com>
2025-09-07 19:26:35 +0700

    feat: implement DisplayInfo method

--- cmd/dungeon-go/main.go
+++ cmd/dungeon-go/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"dungeon-go/internal/models" //importing package models
-	"fmt"
 )
 
 func main() {
 	// Creating a hero using the constructor
 	juggernaut := models.NewHero("Juggernaut")
 
-	fmt.Println(juggernaut)
+	//fmt.Println(juggernaut)
+	juggernaut.DisplayInfo()
 }
