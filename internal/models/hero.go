46448e15066077da8b6349b1b3181ff07ce4a83b
dtt4h <dtt4h322@gmail.com>
2025-09-07 19:29:20 +0700

    feat: implement DisplayInfo method

--- internal/models/hero.go
+++ internal/models/hero.go
@@ -1,5 +1,7 @@
 package models
 
+import "fmt"
+
 type Hero struct {
 	Name       string
 	Health     int
@@ -27,7 +29,10 @@ func (h *Hero) Attack() int {
 
 // Display method
 func (h *Hero) DisplayInfo() {
-
+	fmt.Printf(
+		"\tname: %s\n\thealth: %d\n\tmana: %d\n\tlevel: %d\n\texperience: %d\n",
+		h.Name, h.Health, h.Mana, h.Level, h.Experience,
+	)
 }
 
 // Heal method
