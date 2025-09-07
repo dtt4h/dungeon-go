a90c7f2e1083dd31d3d422ee9d4742ab3582a965
dtt4h <dtt4h322@gmail.com>
2025-09-07 18:32:31 +0700

    feat: add main application entry point

--- cmd/dungeon-go/main.go
+++ cmd/dungeon-go/main.go
@@ -1,47 +1,13 @@
 package main
 
-import "fmt"
-
-//Стуктура героя
-type Hero struct {
-	Name       string
-	Health     int
-	Mana       int
-	Level      int
-	Experience int
-}
-
-//Метод атаки
-func (h *Hero) Attack() int {
-	damage := h.Level * 10
-	h.Experience += 5
-	return damage
-}
-
-func (h *Hero) DisplayInfo() {
-
-}
-
-func (h *Hero) Heal() {
-
-}
-
-//Создание нового героя(конструктор)
-func NewHero(name string) Hero {
-	return Hero{
-		Name:       name,
-		Health:     100,
-		Mana:       100,
-		Level:      1,
-		Experience: 0,
-	}
-}
+import (
+	"dungeon-go/internal/models" //importing package models
+	"fmt"
+)
 
 func main() {
-	//создание героя
-	juggernaut := NewHero("Juggernaut")
-	fmt.Println(juggernaut)
-	damage := juggernaut.Attack()
-	fmt.Printf("%s наносит урон: %d", juggernaut.Name, damage)
+	// Creating a hero using the constructor
+	juggernaut := models.NewHero("Juggernaut")
+
 	fmt.Println(juggernaut)
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module dungeon-go
+
+go 1.25.0
